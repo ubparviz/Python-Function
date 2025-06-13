155a9be6864e2b1b0473a5b4e9e4574fc87d7242
ubparviz <ubparviz@gmail.com>
2025-06-13 09:02:04 +0500

    Done

--- practice07.py
+++ practice07.py
@@ -1,10 +1,12 @@
 # Mini Quiz Game
 
 def check_answer(user_answer, correct_answer):
-    return user_answer.strip().lower() == correct_answer.strip().lower()
+    return user_answer.lower() == correct_answer.lower()
 
-def ask_question(question: str, correct_answer: str):
+def ask_question(question, correct_answer):
+
     user_answer = input(question + " ")
+
     if check_answer(user_answer, correct_answer):
         print("To'g'ri javob!")
     else:
@@ -14,18 +16,12 @@ def main():
     print("Mini Quiz Game\n")
 
     # 1-savol
-    ask_question("1. O'zbekiston poytaxti qaysi shahar?", "Toshkent")
+    ask_question("1. Dunyoda nechta davlat bor?", "227")
 
     # 2-savol
-    ask_question("2. 2 + 2 nechiga teng?", "4")
+    ask_question("2. 2 * 2 + 2=?", "6")
 
     # 3-savol
-    ask_question("3. Dunyoda nechta davlat bor?", "227")
-
-    # 4-savol
-    ask_question("4. 2 * 2 + 2=?", "6")
-
-    # 5-savol
-    ask_question("O'zbekistondagi viloyatlar soni?", "12")
+    ask_question("3. O'zbekistondagi viloyatlar soni?", "12")
 
 main()
