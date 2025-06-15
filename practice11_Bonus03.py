d1046e762694f9cdcd0042585d491de177a31e43
ubparviz <ubparviz@gmail.com>
2025-06-15 14:21:59 +0500

    Done

--- practice11_Bonus03.py
+++ practice11_Bonus03.py
@@ -2,23 +2,40 @@
 # Vazifa: Maoshni kiritadi → soliqni hisoblab beradi.
 # Soliq stavkasi o‘zgarishi mumkin (masalan, >5mln bo‘lsa 20%, boshqacha 13%).
 
-def calculate_soliq(maosh):
-    if maosh > 5_000_000:
-        return maosh * 0.20
-    else:
-        return maosh * 0.13
+def calculate_soliq(maosh: float) -> float:
+    """Maosh qarab soliqni qancha chiqishini hisoblab beradi
 
-def calculate_sof_maosh(maosh):
-    soliq = calculate_soliq(maosh)
-    return maosh - soliq
+    Args:
+        maosh (float): foydalanuvchini maoshi
+    Returns:
+        float: soliq
+    """    
+    if maosh > 5_000_000:
+        soliq = maosh * 0.20
+        return soliq
+    else:
+        soliq = maosh * 0.13
+        return soliq
+
+def calculate_sof_maosh(maosh: float, soliq: float) -> float:
+    """Moashdan soliqni olib tashlaydi. Sof maoshni hisoblaydi.
+    Args:
+        maosh (float): oladigan maosh
+        soliq (float): soliq
+
+    Returns:
+        float: sof maosh
+    """
+    sof_maosh = maosh - soliq   
+    return sof_maosh
 
 def main():
     maosh = float(input("Oylik maoshingizni kiriting (so'mda): "))
-    
-    soliq = calculate_soliq(maosh)
-    sof_maosh = calculate_sof_maosh(maosh)
 
-    print(f"\n Soliq: {soliq} so'm")
-    print(f"\n Sof maosh: {sof_maosh} so'm")
+    soliq = calculate_soliq(maosh)
+    sof_maosh = calculate_sof_maosh(maosh, soliq)
+
+    print(f"\n Soliq: {soliq:.2f} so'm")
+    print(f"\n Sof maosh: {sof_maosh:.2f} so'm")
 
 main()
