4d440ad62fdd59fa29aeab85a458f138bc0ef65c
ubparviz <ubparviz@gmail.com>
2025-06-15 11:50:20 +0500

    Done

--- practice05.py
+++ practice05.py
@@ -1,23 +1,39 @@
 #  Number Guessing Game (Random ishlatilmaydi)
 # Vazifa: Kompyuterda sirli son mavjud. Foydalanuvchi taxmin qiladi. To‘g‘ri yoki xato deyilgan bo‘ladi.
 
-def check_guess(secret, guess):
-    return secret == guess
+# Global O'zgaruvchi
+secret_number = 69
 
-def print_result(is_correct):
+def check_guess(guess: int) -> bool:
+    """Sirli son bilan foydalanuvchi kiritgan son tengmi tekshiradi
+
+    Args:
+        guess (int): butun son qabul qiladi
+
+    Returns:
+        bool: Tekshiradi, TRUE yoki FALSE chiqaradi
+    """    
+    return secret_number == guess
+
+def print_result(is_correct: bool) -> None:
+    """Foydalanuvchi topdimi yoki yo'q aniqlashtiradi
+
+    Args:
+        is_correct (bool): True yoki False qabul qiladi
+    
+    Returns: None
+    """    
     if is_correct:
         print("To'g'ri topdingiz!")
     else:
         print("Xato")
 
-def main():
-    
-    secret_number = 69
+def main() -> None:
+    """Barcha funskiyalarni ishga tushiradi foydalanuvchi to'g'ri sonni topmaganchu bu funksiya to'xtamaydi"""    
     while True:
-
         guess = int(input("Sirli sonni toping (1 dan 100 gacha): "))
 
-        is_correct = check_guess(secret_number, guess)
+        is_correct = check_guess(guess)
         print_result(is_correct)
 
         if is_correct:
--- practice06.py
+++ practice06.py
@@ -1,14 +1,28 @@
 # Telefon raqam 9 ta raqamdan iboratligini tekshiradi.
 
-def is_valid_phone_number(phone):
+def is_valid_phone_number(phone: str):
+    """
+    Telefon raqam to'g'ri kiritilganini tekshiradi.
+
+    Args:
+        phone (str): Foydalanuvchi tomonidan kiritilgan telefon raqam (faqat raqamlardan iborat bo'lishi kerak).
+
+    Result:
+        Raqam 9 xonali bo'lsa — to'g'ri deb chiqaradi,
+        Aks holda — xatolik xabari beradi.
+
+    Returns: None
+    """
 
     if phone.isdigit() and len(phone) == 9:
-        return print("Raqam to'g'ri kiritildi")
+        print("Raqam to'g'ri kiritildi")
     
     else:
-        return  print("Telefon raqam 9 ta raqamdan iborat bo'lishi kerak")
+        print("Telefon raqam 9 ta raqamdan iborat bo'lishi kerak")
 
 def main():
+    """Foydalanuvchidan string so'raydi va uni is_valid_phone_number orqali tekshiradi"""
+    
     phone = input("Telefon raqamingizni kiriting (991234567): ")
 
     is_valid_phone_number(phone)
